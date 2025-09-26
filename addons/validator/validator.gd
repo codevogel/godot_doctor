a53fd7f9307d42fa8e68dd79c6f31a65c5ee3ac5
Kamiel de Visser <kamieldevisser@gmail.com>
2025-09-26 14:56:01 +0200

    add validation global

--- addons/validator/validator.gd
+++ addons/validator/validator.gd
@@ -1,15 +1,18 @@
 @tool
 extends EditorPlugin
 
+const VALIDATION_GLOBAL_NAME := "Validation"
+const VALIDATION_GLOBAL_PATH := "res://addons/validator/validation.gd"
+
 
 func _enable_plugin() -> void:
 	# Add autoloads here.
-	pass
+	add_autoload_singleton(VALIDATION_GLOBAL_NAME, VALIDATION_GLOBAL_PATH)
 
 
 func _disable_plugin() -> void:
 	# Remove autoloads here.
-	pass
+	remove_autoload_singleton(VALIDATION_GLOBAL_NAME)
 
 
 func _enter_tree() -> void:

358e7958ddba5f0f06c6eb22f4aa0f72e78210f0
Kamiel de Visser <kamieldevisser@gmail.com>
2025-09-28 15:51:53 +0200

    add settings resource

--- addons/validator/validator_plugin.gd
+++ addons/validator/validator_plugin.gd
@@ -1,61 +1,69 @@
 @tool
 extends EditorPlugin
 
-const VALIDATING_METHOD_NAME := "_get_validation_conditions"
+const VALIDATING_METHOD_NAME: String = "_get_validation_conditions"
+const VALIDATOR_DOCK_SCENE_PATH: String = "res://addons/validator/dock/validator_dock.tscn"
+const VALIDATOR_SETTINGS_PATH: String = "res://addons/validator/settings/validator_settings.tres"
 var dock: ValidatorDock
+
+var settings: ValidatorSettings:
+	get:
+		if not settings:
+			settings = load(VALIDATOR_SETTINGS_PATH) as ValidatorSettings
+		return settings
 
 
 func _enable_plugin() -> void:
-	print("[VALIDATOR] Enabling plugin...")
-	# Add autoloads here.
+	print_debug("[VALIDATOR] Enabling plugin...")
 
 
 func _disable_plugin() -> void:
-	print("[VALIDATOR] Disabling plugin...")
-	# Remove autoloads here.
+	print_debug("[VALIDATOR] Disabling plugin...")
 
 
 func _enter_tree():
-	print("[VALIDATOR] Entering tree...")
-	scene_saved.connect(_on_scene_saved)
-	dock = preload("res://addons/validator/dock/validator_dock.tscn").instantiate() as ValidatorDock
+	print_debug("[VALIDATOR] Entering tree...")
+	_connect_signals()
+	dock = preload(VALIDATOR_DOCK_SCENE_PATH).instantiate() as ValidatorDock
 	add_control_to_dock(DOCK_SLOT_LEFT_UL, dock)
 
 
 func _exit_tree():
-	print("[VALIDATOR] Exiting tree...")
-	# Clean-up of the plugin goes here.
-	# Remove the dock.
+	print_debug("[VALIDATOR] Exiting tree...")
+	_disconnect_signals()
+	_remove_dock()
+
+
+func _connect_signals():
+	print_debug("[VALIDATOR] Connecting signals...")
+	scene_saved.connect(_on_scene_saved)
+
+
+func _disconnect_signals():
+	print_debug("[VALIDATOR] Disconnecting signals...")
 	if scene_saved.is_connected(_on_scene_saved):
 		scene_saved.disconnect(_on_scene_saved)
+
+
+func _remove_dock():
 	remove_control_from_docks(dock)
-	# Erase the control from the memory.
 	dock.free()
 
 
-func get_all_children_that_are_tree_recursive(node: Node) -> Array:
-	var labels: Array = []
-	if node is Tree:
-		labels.append(node)
-	for child in node.get_children():
-		labels.append_array(get_all_children_that_are_tree_recursive(child))
-	return labels
-
-
 func _on_scene_saved(file_path: String) -> void:
-	print("[VALIDATOR] Scene saved.")
+	print_debug("[VALIDATOR] Scene saved: %s" % file_path)
 	var current_edited_scene_root: Node = get_editor_interface().get_edited_scene_root()
 	if not is_instance_valid(current_edited_scene_root):
-		print("[Validator] No current edited scene root. Skipping validation.")
+		print_debug("[VALIDATOR] No current edited scene root. Skipping validation.")
 		return
 
 	dock.clear_errors()
 
 	var nodes_to_validate: Array = _find_nodes_to_validate_in_tree(current_edited_scene_root)
-	print("[Validator] Found %d nodes to validate." % nodes_to_validate.size())
+	print_debug("[Validator] Found %d nodes to validate." % nodes_to_validate.size())
 
 	for node: Node in nodes_to_validate:
-		print("[Validator] Calling %s on node: %s" % [VALIDATING_METHOD_NAME, node.name])
+		print_debug("[Validator] Calling %s on node: %s" % [VALIDATING_METHOD_NAME, node.name])
 
 		var validation_target: Object = node
 		var script: Script = node.get_script()
@@ -83,10 +91,10 @@ func _on_scene_saved(file_path: String) -> void:
 		if validation_target.has_method(VALIDATING_METHOD_NAME):
 			# The script is loaded, and method call should now work.
 			var generated_conditions = validation_target.call(VALIDATING_METHOD_NAME)
-			print("[Validator] Generated validation conditions: %s" % [generated_conditions])
+			print_debug("[Validator] Generated validation conditions: %s" % [generated_conditions])
 			var validation_result = ValidationResult.new(generated_conditions)
 			for error in validation_result.errors:
-				print("[Validator] Validation error in node %s: %s" % [node.name, error])
+				print_debug("[Validator] Validation error in node %s: %s" % [node.name, error])
 				dock.add_to_dock(
 					node, "[b]Configuration warning in %s:[/b]\n%s" % [node.name, error]
 				)
@@ -107,6 +115,8 @@ func _on_scene_saved(file_path: String) -> void:
 			validation_target.free()
 
 
+## Finds all nodes in the tree that implement the VALIDATING_METHOD_NAME method recursively.
+## Returns an array of nodes that implement the VALIDATING_METHOD_NAME method.
 func _find_nodes_to_validate_in_tree(node: Node) -> Array:
 	var nodes_to_validate: Array = []
 
@@ -116,3 +126,8 @@ func _find_nodes_to_validate_in_tree(node: Node) -> Array:
 	for child in node.get_children():
 		nodes_to_validate.append_array(_find_nodes_to_validate_in_tree(child))
 	return nodes_to_validate
+
+
+func _print_debug(message: String) -> void:
+	if settings.show_debug_prints:
+		print("[VALIDATOR] %s" % message)

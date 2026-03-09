38a0a2f7e949ed799d895c6a643e0291329f8902
Kamiel de Visser <kamieldevisser@gmail.com>
2026-03-09 10:05:32 +0100

    run gdformat

--- addons/godot_doctor/cli/primitives/reports/message_counts.gd
+++ addons/godot_doctor/cli/primitives/reports/message_counts.gd
@@ -6,15 +6,16 @@ var hard_error: int = 0
 var warnings_as_errors: int = 0
 
 var total: int:
-    get:
-        return info + warning + hard_error
+	get:
+		return info + warning + hard_error
 
 var total_errors: int:
-    get:
-        return hard_error + warnings_as_errors
+	get:
+		return hard_error + warnings_as_errors
+
 
 func add(other: MessageCounts) -> void:
-    info += other.info
-    warning += other.warning
-    hard_error += other.hard_error
-    warnings_as_errors += other.warnings_as_errors
+	info += other.info
+	warning += other.warning
+	hard_error += other.hard_error
+	warnings_as_errors += other.warnings_as_errors
